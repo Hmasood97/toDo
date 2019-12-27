c0c2b5cd30e22adde274bf68bcc52daebe5c21bf
Hmasood97 <hisham9977@gmail.com>
2019-12-27 14:45:43 -0500

    second

--- toDo.py
+++ toDo.py
@@ -1,1 +1,21 @@
-import tkinter
+import Tkinter
+
+import random
+
+#Creating root window
+root = Tkinter.Tk()
+
+
+lbl_title = Tkinter.Label(root, text= "To-Do-List", bg="white" )
+lbl_title.pack()
+
+
+lbl_display = Tkinter.Label(root, text ="", bg="white")
+lbl_display.pack()
+
+
+
+
+#runs it
+
+root.mainloop()
