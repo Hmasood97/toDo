18a80400b9bdceb1059a13d29ffbfe0ae765c290
Hmasood97 <hisham9977@gmail.com>
2019-12-27 21:58:30 -0500

    third

--- toDo.py
+++ toDo.py
@@ -5,14 +5,31 @@ import random
 #Creating root window
 root = Tkinter.Tk()
 
+# Function Definitions
 
+
+
+
+
+
+
+
+
+#title
 lbl_title = Tkinter.Label(root, text= "To-Do-List", bg="white" )
 lbl_title.pack()
 
-
+#display
 lbl_display = Tkinter.Label(root, text ="", bg="white")
 lbl_display.pack()
 
+#Text input 
+txt_input = Tkinter.Entry(root, width=15)
+txt_input.pack()
+
+#add Task button
+btn_add_task = Tkinter.Button(root, text = "Add Task", fg = "green", bg= "white")
+btn_add_task.pack()
 
 
 
